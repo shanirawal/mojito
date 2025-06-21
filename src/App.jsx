1a528d13a8a1e5686fd74d0a8240ba4f3263e85a
shanirawal <shanirawal5@gmail.com>
2025-06-21 17:30:32 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ScrollTrigger , SplitText } from 'gsap/all';
+import gsap from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+const App = () => {
+  return (
+    <div >
+      shani
+    </div>
+  )
+}
+
+export default App
